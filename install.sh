4ac6b88dd7047584896c42a922b6fe771f8b84ad
Biswajit <bksinha4497@gmail.com>
2021-03-21 15:53:50 +0530

    create install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,8 @@ ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
 hwclock --systohc 
 echo "LANG=en_US.UTF-8" >>/etc/locale.gen 
 locale-gen 
-echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "en_US.UTF-8" >> /etc/locale.conf
+echo "en_IN UTF-8" >> /etc/locale.conf
 echo "arch" >> /etc/hostname 
 echo "127.0.0.1	localhost" >>etc/hosts
 echo "::1	localhost" >>etc/hosts
