66fc2535b3708f3fa9b55de5f10e1ab36d055271
bksinha4497 <bksinha4497@gmail.com>
2021-03-21 16:02:10 +0530

    create install.sh

--- install.sh
+++ install.sh
@@ -18,37 +18,6 @@ echo root:password | chpasswd
 
 pacman -S --no-confirm bridge-utils wpa_supplicant wireless_tools networkmanager nm-connection-editor network-manager-applet dhcpcd  openssh nmctl git wget firewalld vim ntfs-3g terminus-font reflector rsync nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pulseaudio bash-completion acpi acpi_call tlp cockpit cockpit-machines qemu qemu-arch-extra ovmf dnsmasq nvim grub efibootmgr
 
-# pacman -S --noconfirm xf86-video-amdgpu
-# pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
-
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
-grub-mkconfig -o /boot/grub/grub.cfg
-
-systemctl enable NetworkManager
-systemctl enable bluetooth
-systemctl enable cups
-systemctl enable sshd
-systemctl enable avahi-daemon
-systemctl enable tlp
-systemctl enable reflector.timer
-systemctl enable fstrim.timer
-systemctl enable libvirtd
-systemctl enable firewalld
-systemctl enable acpid
-
-useradd -m ermanno
-echo ermanno:password | chpasswd
-usermod -aG libvirt ermanno
-
-echo "ermanno ALL=(ALL) ALL" >> /etc/sudoers.d/ermanno
-
-
-/bin/echo -e "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
-
-
-#pacman -S --no-confirm nvidia nvidia-settings nvidia-utils
-#pacman -S --no-confirm nvidia xf86-video-amdgpu
-
 mkinitcpio -P 
 
 mkdir /boot/efi
