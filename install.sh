df0d1bb81c4c05bfc02358970176bd778d53d2d3
Biswajit Kumar <bksinha4497@gmail.com>
2021-03-22 21:48:39 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -60,7 +60,7 @@ echo "127.0.1.1	arch.localdomain arch" >>etc/hosts
 echo root:password | chpasswd
 
 echo "Installing lot of softwares"
-pacman -S --noconfirm btrfs-progs bridge-utils wpa_supplicant wireless_tools networkmanager nm-connection-editor network-manager-applet dhcpcd  openssh nmctl git wget firewalld vim ntfs-3g terminus-font reflector rsync nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pulseaudio bash-completion acpi acpi_call tlp cockpit cockpit-machines qemu qemu-arch-extra ovmf dnsmasq nvim grub efibootmgr
+pacman -S --noconfirm btrfs-progs bridge-utils wpa_supplicant wireless_tools networkmanager nm-connection-editor network-manager-applet dhcpcd  openssh git wget firewalld vim ntfs-3g terminus-font reflector rsync nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pulseaudio bash-completion acpi acpi_call tlp cockpit cockpit-machines qemu qemu-arch-extra ovmf dnsmasq vim grub efibootmgr
 
 echo "Generating initramfs"
 mkinitcpio -P 
