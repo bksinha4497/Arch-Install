aac5d216c9d2f29614b9869647239a0949f2af8e
bksinha4497 <bksinha4497@gmail.com>
2021-03-23 15:39:22 +0530

    Created script to isntall desktop environment

---
+++ desktop-env-install.bash
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+gnome_install() {
+  echo "Installing Gnome"
+  sudo pacman -S --noconfirm xorg gdm gnome gnome-extra gnome-tweaks firefox vlc
+  sudo systemctl enable gdmi
+  echo "Installation complete , you can reboot now"
+}
+
+kde_install() {
+  echo "Installing KDE"
+  sudo pacman -S --noconfirm xorg sddm plasma kde-applications firefox vlc
+  sudo systemctl enable sddm
+  echo "Installation compelte , you can reboot now"
+}
+
+press_enter() {
+  echo ""
+  echo -n "	Press Enter to continue "
+  read
+  clear
+}
+
+incorrect_selection() {
+  echo "Incorrect selection! Try again."
+}
+
+until [ "$selection" = "0" ]; do
+  clear
+  echo ""
+  echo "    	1  -  Install Gnome 1"
+  echo "    	2  -  Install Kde"
+  echo "        3  -  Reboot"
+  echo "    	0  -  Exit"
+  echo ""
+  echo -n "  Enter selection: "
+  read selection
+  echo ""
+  case $selection in
+    1 ) clear ; gnome_install ; press_enter ;;
+    2 ) clear ; kde_install ; press_enter ;;
+    3 ) clear ; reboot;;
+    0 ) clear ; exit ;;
+    * ) clear ; incorrect_selection ; press_enter ;;
+  esac
+done
