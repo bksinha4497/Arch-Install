b87272c871a1829a83ab066785025f28cec00079
bksinha4497 <bksinha4497@gmail.com>
2021-03-27 21:54:35 +0530

    update base-install

--- base-install/base-install.bash
+++ base-install/base-install.bash
@@ -39,43 +39,32 @@ fi
 
 btrfs subvolume create /mnt/@
 btrfs subvolume create /mnt/@home
-btrfs subvolume create /mnt/@snapshots
+btrfs subvolume create /mnt/@root
+btrfs subvolume create /mnt/@srv
+btrfs subvolume create /mnt/@cache
+btrfs subvolume create /mnt/@log
+btrfs subvolume create /mnt/@tmp
 
 umount -R /mnt
 
-if mount | grep /mnt > /dev/null; then
-    echo "unablt to auto unmount /mnt"
-else
-    echo "unmount /mnt"
-fi
 
 mkdir /mnt/home
-mkdir /mnt/.snapshots
+mkdir /mnt/root
+mkdir /mnt/srv
+mkdir /mn/var/cache
+mkdir /mnt/var/log
+mkdir /mnt/var/tmp
 
 echo "Mounting BTRFS subvolumes"
 o=defaults,x-mount.mkdir
 o_btrfs=$o,compress=zstd,ssd,noatime,autodefrag,rw,space_cache
 mount -t btrfs -o subvol=@,$o_btrfs LABEL=system /mnt  
 mount -t btrfs -o subvol=@home,$o_btrfs LABEL=system /mnt/home 
-mount -t btrfs -o subvol=@snapshots,$o_btrfs LABEL=system /mnt/.snapshots 
-
-if mount | grep /mnt > /dev/null; then
-    echo "/mnt is mounted"
-else
-    echo "/mnt is not mounted"
-fi
-
-if mount | grep /mnt/home > /dev/null; then
-    echo "/mnt/home is mounted"
-else
-    echo "/mnt/home is not mounted"
-fi
-
-if mount | grep /mnt/.snapshots > /dev/null; then
-    echo "/mnt/snapshots is mounted"
-else
-    echo "/mnt/.snapshots is not mounted"
-fi
+mount -t btrfs -o subvol=@root,$o_btrfs LABEL=system /mnt/root 
+mount -t btrfs -o subvol=@srv,$o_btrfs LABEL=system /mnt/srv 
+mount -t btrfs -o subvol=@cache,$o_btrfs LABEL=system /mnt/var/cache 
+mount -t btrfs -o subvol=@log,$o_btrfs LABEL=system /mnt/var/log 
+mount -t btrfs -o subvol=@tmp,$o_btrfs LABEL=system /mnt/var/tmp 
 
 echo "installing arch base"
 pacstrap /mnt base linux-zen linux-firmware intel-ucode base-devel 
