348444b77e40eb0a73bbb8de9801c637e339272f
Biswajit Kumar <bksinha4497@gmail.com>
2021-03-27 10:08:32 +0530

    Update chroot-install.bash

--- base-install/chroot-install.bash
+++ base-install/chroot-install.bash
@@ -59,8 +59,7 @@ echo "Updating sudo"
 pacman --sync sudo
 
 echo "Adding user \"biswajit\" with default root and user password as password"
-useradd -G wheel,power,audio,video -m bksinha4497
-usermod -aG libvirt biswajit
+usermod -aG wheel,power,audio,video,libvirt biswajit
 sed -i '0,/# %wheel/s// %wheel/' /etc/sudoers
 echo biswajit:password | chpasswd
 
