41561941f43fee36c29b8236cb7a3a21aed4625b
Biswajit Kumar <bksinha4497@gmail.com>
2021-11-29 09:59:19 +0530

    Update base-install.bash

--- base-install/base-install.bash
+++ base-install/base-install.bash
@@ -81,9 +81,9 @@ echo "Generating fstab"
 genfstab -U /mnt >> /mnt/etc/fstab  
 
 echo "chrooting into arch installed system"
-cp ~/Arch-Install/base-install/chroot-install.bash /mnt/
-chmod u+x /mnt/chroot-install.bash
-arch-chroot /mnt ./chroot-install.bash
+cp -r ~/Arch-Install /mnt/
+chmod u+x /mnt/Arch-Install/base-install/*
+arch-chroot /mnt ./mnt/Arch-Install/base-install/chroot-install.bash
 
 umount -R /mnt
 
