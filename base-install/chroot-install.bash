5b05a65c8896e924b44c347abcf96d5e202b3bec
Biswajit Kumar <bksinha4497@gmail.com>
2021-12-01 19:07:26 +0530

    Update chroot-install.bash

--- base-install/chroot-install.bash
+++ base-install/chroot-install.bash
@@ -30,10 +30,11 @@ pacman -S --noconfirm nvidia-dkms nvidia-utils nvidia-settings
 
 # Install Optimus for hybrid graphics
 echo "Installing optimus manager for hybrid graphics"
-wget https://aur.archlinux.org/cgit/aur.git/snapshot/optimus-manager.tar.gz
-wget https://aur.archlinux.org/cgit/aur.git/snapshot/optimus-manager-qt.tar.gz
-wget https://aur.archlinux.org/cgit/aur.git/snapshot/bbswitch-dkms-git.tar.gz
-pacman -U optimus-manager-tar.gz optimus-manager-qt.tar.gz bbswitch-dkms-git.tar.gz
+pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+pacman-key --lsign-key 3056513887B78AEB
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+echo "[chaotic-aur]" >> /etc/pacman.conf && echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
+pacman -Sy optimus-manager optimus-manager-qt bbswitch-dkms-git
 
 echo "Generating initramfs"
 mkinitcpio -P 
