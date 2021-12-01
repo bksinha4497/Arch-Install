8078b698fed6b671f0c6963c989990a7958a136f
Biswajit Kumar <bksinha4497@gmail.com>
2021-12-01 23:38:48 +0530

    Update chroot-install.bash

--- base-install/chroot-install.bash
+++ base-install/chroot-install.bash
@@ -45,8 +45,13 @@ mkdir /boot/efi
 mount LABEL=EFI /boot/efi
 
 echo "Installing Grub"
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --boot-directory=/boot/efi --bootloader-id=grub --removable
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB --recheck
 grub-mkconfig -o /boot/grub/grub.cfg
+
+echo "Setting up grub boot loader to run startup.nsh file correctly during boot"
+mkdir /boot/efi/EFI/BOOT
+cp /boot/efi/EFI/GRUB/grubx64.efi /boot/efi/EFI/BOOT/BOOTx64.EFI
+echo "bcf boot add 1 fs0:\EFI\GRUB\grubx64.efi "GRUB BOOT LOADER"" >>/boot/efi/startup.nsh
 
 echo "Enabelling services to start on boot"
 systemctl enable NetworkManager 
