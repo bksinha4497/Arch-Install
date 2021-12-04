4dc55d3f614add4b22faa9a28434e79f809c3735
Biswajit Kumar <bksinha4497@gmail.com>
2021-12-04 12:08:27 +0530

    Update base-install.bash

--- base-install/base-install.bash
+++ base-install/base-install.bash
@@ -40,7 +40,8 @@ umount -R /mnt
 
 sleep 1
 
-mkdir /mnt/{boot,home,root,srv,var,var/cache,var/log,var/tmp,.snapshots}
+mkdir /mnt/{boot,home,root,srv,var,var/cache,var/log,var/tmp}
+#mkdir /mnt/.snapshots
 
 echo "Mounting BTRFS subvolumes"
 o=defaults,x-mount.mkdir
@@ -52,7 +53,7 @@ mount -t btrfs -o subvol=@srv,$o_btrfs LABEL=system /mnt/srv
 mount -t btrfs -o subvol=@cache,$o_btrfs LABEL=system /mnt/var/cache 
 mount -t btrfs -o subvol=@log,$o_btrfs LABEL=system /mnt/var/log 
 mount -t btrfs -o subvol=@tmp,$o_btrfs LABEL=system /mnt/var/tmp 
-mount -t btrfs -o subvol=@snapshots,$o_btrfs LABEL=system /mnt/.snapshots 
+#mount -t btrfs -o subvol=@snapshots,$o_btrfs LABEL=system /mnt/.snapshots 
 
 echo "Installing arch base"
 pacstrap /mnt base base-devel linux-zen linux-zen-headers linux-firmware
