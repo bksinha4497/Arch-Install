97d25d0c2e6b991148dfb2b02802e4d75e98364f
Biswajit Kumar <bksinha4497@gmail.com>
2021-12-05 00:28:59 +0530

    Update chroot-install.bash

--- base-install/chroot-install.bash
+++ base-install/chroot-install.bash
@@ -29,12 +29,6 @@ pacman -Sy --noconfirm intel-ucode xf86-video-intel reflector btrfs-progs snappe
 #pacman -S --noconfirm nvidia-dkms nvidia-utils nvidia-settings
 
 # Install Optimus for hybrid graphics
-#echo "Installing optimus manager for hybrid graphics"
-#pacman-key --init
-#pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-#pacman-key --lsign-key 3056513887B78AEB
-#pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-#echo "[chaotic-aur]" >> /etc/pacman.conf && echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
 #pacman -Sy optimus-manager optimus-manager-qt bbswitch-dkms-git
 
 echo "Generating initramfs"
