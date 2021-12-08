ae8f2cb83d4fb81d308e69f53a5765936e50c32b
Biswajit Kumar <bksinha4497@gmail.com>
2021-12-08 19:26:17 +0530

    Update base-install.bash

--- base-install/base-install.bash
+++ base-install/base-install.bash
@@ -44,7 +44,7 @@ mkdir /mnt/{boot,home,root,srv,var,var/cache,var/log,var/tmp,.snapshots}
 
 echo "Mounting BTRFS subvolumes"
 o=defaults,x-mount.mkdir
-o_btrfs=$o,compress=zstd,ssd,noatime,autodefrag,rw,space_cache
+o_btrfs=$o,compress=zstd,ssd,noatime,autodefrag,rw,space_cache=v2
 mount -t btrfs -o subvol=@,$o_btrfs LABEL=system /mnt  
 mount -t btrfs -o subvol=@home,$o_btrfs LABEL=system /mnt/home 
 mount -t btrfs -o subvol=@root,$o_btrfs LABEL=system /mnt/root 
