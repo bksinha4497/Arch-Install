6bb10488a7b81f2e63e014e51ed8970c177b109a
Biswajit Kumar <bksinha4497@gmail.com>
2024-06-27 10:36:50 +0530

    Update chroot-install.bash

--- base-install/chroot-install.bash
+++ base-install/chroot-install.bash
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 
 # Function to handle errors
 handle_error() {
@@ -10,17 +10,22 @@ handle_error() {
 trap 'handle_error $LINENO' ERR
 
 echo "Chrooted into Arch and Setting up base system"
+
+# Set timezone
 ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime  
 hwclock --systohc 
 
+# Set locales
 echo "Setting locales"
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 locale-gen
 
+# Set keymap
 echo "Adding persistent keymap"
 echo "KEYMAP=us" > /etc/vconsole.conf
 
+# Set hostname and hosts file
 echo "Setting hosts and hostname"
 echo "arch" > /etc/hostname 
 cat <<EOF > /etc/hosts
@@ -29,27 +34,78 @@ cat <<EOF > /etc/hosts
 127.0.1.1   arch.localdomain arch
 EOF
 
-echo "Setting default root passwd as password"
+# Set root password
+echo "Setting default root password"
 echo root:password | chpasswd
 
-echo "Installing lot of software"
-pacman -Sy --noconfirm intel-ucode xf86-video-intel reflector btrfs-progs snapper snap-pac grub efibootmgr grub-btrfs bridge-utils wpa_supplicant wireless_tools networkmanager nm-connection-editor network-manager-applet dhcpcd openssh wget git ntfs-3g exfat-utils reflector rsync nfs-utils inetutils dnsutils bluez bluez-utils alsa-utils pipewire gst-plugin-pipewire pipewire-pulse pipewire-alsa pipewire-jack virt-manager libvirt qemu qemu-arch-extra ovmf dnsmasq vim neofetch ghostscript libreoffice-fresh vlc zsh
+# Install necessary software packages
+echo "Installing necessary software"
+pacman -Sy --noconfirm \
+    intel-ucode \
+    xf86-video-intel \
+    reflector \
+    btrfs-progs \
+    snapper \
+    snap-pac \
+    grub \
+    efibootmgr \
+    grub-btrfs \
+    bridge-utils \
+    wpa_supplicant \
+    wireless_tools \
+    networkmanager \
+    nm-connection-editor \
+    network-manager-applet \
+    dhcpcd \
+    openssh \
+    wget \
+    git \
+    ntfs-3g \
+    exfat-utils \
+    reflector \
+    rsync \
+    nfs-utils \
+    inetutils \
+    dnsutils \
+    bluez \
+    bluez-utils \
+    alsa-utils \
+    pipewire \
+    gst-plugin-pipewire \
+    pipewire-pulse \
+    pipewire-alsa \
+    pipewire-jack \
+    virt-manager \
+    libvirt \
+    qemu-full \
+    ovmf \
+    dnsmasq \
+    vim \
+    neofetch \
+    ghostscript \
+    libreoffice-fresh \
+    vlc \
+    zsh
 
-# Install Nvidia Drivers (uncomment if needed)
+# Optionally install Nvidia drivers
 #echo "Installing nvidia drivers"
 #pacman -S --noconfirm nvidia-dkms nvidia-utils nvidia-settings nvidia-prime
 
+# Generate initramfs
 echo "Generating initramfs"
 mkinitcpio -P 
 
-echo "Creating directory /boot/efi and Mounting EFI partition to /boot/efi"
-mkdir /boot/efi
+# Mount EFI partition
+echo "Creating directory /boot/efi and mounting EFI partition"
+mkdir -p /boot/efi
 mount LABEL=EFI /boot/efi
 
-echo "Installing Grub"
+# Install Grub bootloader
+echo "Installing Grub bootloader"
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB --recheck
 grub-mkconfig -o /boot/grub/grub.cfg
 
+# Enable services to start on boot
 echo "Enabling services to start on boot"
 systemctl enable NetworkManager 
 systemctl enable dhcpcd
@@ -58,33 +114,37 @@ systemctl enable bluetooth
 systemctl enable libvirtd
 systemctl enable reflector.timer
 systemctl enable grub-btrfs.path
-#systemctl enable optimus-manager
+# systemctl enable optimus-manager (uncomment if needed)
 
-echo "Updating sudo" 
+# Update sudoers
+echo "Updating sudo"
 pacman --noconfirm --sync sudo
 
-echo "Adding user \"biswajit\" with default root and user password as password"
-useradd -G wheel,power,audio,video,storage,libvirt,kvm,cups -d /home/biswajit -m biswajit
-sed -i '0,/# %wheel/s// %wheel/' /etc/sudoers
-echo biswajit:password | chpasswd
+# Add a user and set permissions
+echo "Adding user 'biswajit' and setting permissions"
+useradd -G wheel,power,audio,video,storage,libvirt,kvm,cups -m biswajit
+sed -i 's/^# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
+echo "biswajit:password" | chpasswd
 
-echo "Updating zsh as default shell"
-chsh -s /usr/bin/zsh
+# Set zsh as default shell for the user
+echo "Setting zsh as default shell for user 'biswajit'"
 chsh -s /usr/bin/zsh biswajit
 
+# Update mirrorlist
 echo "Updating mirrorlist"
 reflector -c "India" -f 5 > /etc/pacman.d/mirrorlist
 
-#echo "Adding Nvidia Hook"
+# Optionally add Nvidia hook
 #cp /Arch-Install/nvidia.hook /etc/pacman.d/hooks/
 
-echo "Adding grub hook - run grub-mkconfig when new linux kernel is installed or updated or removed"
+# Add grub hook for kernel updates
+echo "Adding grub hook for kernel updates"
 cp /Arch-Install/grub.hook /usr/share/libalpm/hooks/
 
+# Create snapper configuration
 echo "Creating snapper config"
 snapper -c root create-config /
 
-sleep 1s
-echo "Exiting out of chroot"
-sleep 1s
+# Exit chroot environment
+echo "Exiting chroot environment"
 exit
